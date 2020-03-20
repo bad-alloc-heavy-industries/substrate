ddb29b9c7246227e701415564524256845cd9098
dx-mon <dx-mon@users.sourceforge.net>
2020-03-20 15:22:28 +0000

    Fixed up windows' dumb crash-on-invalid-param stuff that will murder the tests if not disabled

--- test/test.cxx
+++ test/test.cxx
@@ -1,2 +1,21 @@
-#define CATCH_CONFIG_MAIN
+#ifdef _WINDOWS
+#include <cstdlib>
+#include <crtdbg.h>
+#endif
+#define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+
+#ifdef _WINDOWS
+void invalidHandler(const wchar_t *, const wchar_t *, const wchar_t *, const uint32_t, const uintptr_t) { }
+#endif
+
+int main(int argCount, char **argList)
+{
+#ifdef _WINDOWS	
+	_set_invalid_parameter_handler(invalidHandler);
+	_CrtSetReportMode(_CRT_ASSERT, 0);
+	_CrtSetReportMode(_CRT_ERROR, 0);	
+#endif
+
+	return Catch::Session().run(argCount, argList);
+}
