2f8b78979d5fc8e9ee8d515c332f2e63a984c96d
dx-mon <dx-mon@users.sourceforge.net>
2020-03-20 22:38:13 +0000

    Fixed a test workdir issue, and began fleshing out the constants for the fd_t tests along with the logic for the read test

--- test/fd.cxx
+++ test/fd.cxx
@@ -1,14 +1,29 @@
+#include <cstdint>
 #include <memory>
 #include <array>
 #include <substrate/fd>
 #include <catch.hpp>
 
+constexpr static std::array<char, 4> testArray{'t', 'E', 'S', 't'};
+constexpr static char testChar{'.'};
+static std::string testString{"fileDescriptor"};
+
+UINT8_C();
+constexpr static auto u8{uint8_t(0x5A)};
+constexpr static auto i8{int8_t(0xA5)};
+constexpr static auto u16{uint16_t(0x125A)};
+constexpr static auto i16{int16_t(0x12A5)};
+constexpr static auto u32{uint32_t(UINT32_C(0x1234565A))};
+constexpr static auto i32{int32_t(INT32_C(0x123456A5))};
+constexpr static auto u64{uint64_t(UINT64_C(0x123456789ABCDE5A))};
+constexpr static auto i64{int64_t(INT64_C(0x123456789ABCDEA5))};
+
 TEST_CASE("fd_t bad fd", "[fd_t]")
 {
 	fd_t file{};
 	REQUIRE(file == -1);
-	REQUIRE(!file.valid());
-	REQUIRE(!file.isEOF());
+	REQUIRE_FALSE(file.valid());
+	REQUIRE_FALSE(file.isEOF());
 
 	REQUIRE(file.read(nullptr, 0, nullptr) == -1);
 	REQUIRE(file.write(nullptr, 0) == -1);
@@ -18,14 +33,43 @@ TEST_CASE("fd_t bad open", "[fd_t]")
 {
 	fd_t file{"bad.file", O_RDONLY};
 	REQUIRE(file == -1);
-	REQUIRE(!file.valid());
-	REQUIRE(!file.isEOF());
+	REQUIRE_FALSE(file.valid());
+	REQUIRE_FALSE(file.isEOF());
 }
 
 TEST_CASE("fd_t write", "[fd_t]")
 {
+	fd_t file{"fd.test", O_WRONLY | O_CREAT | O_EXCL, normalMode};
+	REQUIRE(file >= 0);
+	REQUIRE(file.valid());
+	REQUIRE_FALSE(file.isEOF());
+
+	//
+}
+
+template<typename T> void read(fd_t &file, const T &expected)
+{
+	T result{};
+	REQUIRE(file.read(result));
+	REQUIRE(result == expected);
 }
 
 TEST_CASE("fd_t read", "[fd_t]")
 {
+	fd_t file{"fd.test", O_RDONLY};
+	REQUIRE(file >= 0);
+	REQUIRE(file.valid());
+	REQUIRE_FALSE(file.isEOF());
+
+	read(file, testArray);
+	read(file, testChar);
+	//read(file, testString);
+	read(file, u8);
+	read(file, i8);
+}
+
+ANON_TEST_CASE()
+{
+	unlink("fd.test");
+	SUCCEED();
 }
