82f3e527aa8a17b3ba2d42caeef44af0c766d73f
dx-mon <dx-mon@users.sourceforge.net>
2020-03-20 19:15:17 +0000

    Cleaned up a bit and fixed a missing default_options for the build

--- test/fd.cxx
+++ test/fd.cxx
@@ -1,3 +1,5 @@
+#include <memory>
+#include <array>
 #include <substrate/fd>
 #include <catch.hpp>
 
@@ -10,4 +12,20 @@ TEST_CASE("fd_t bad fd", "[fd_t]")
 
 	REQUIRE(file.read(nullptr, 0, nullptr) == -1);
 	REQUIRE(file.write(nullptr, 0) == -1);
+}
+
+TEST_CASE("fd_t bad open", "[fd_t]")
+{
+	fd_t file{"bad.file", O_RDONLY};
+	REQUIRE(file == -1);
+	REQUIRE(!file.valid());
+	REQUIRE(!file.isEOF());
+}
+
+TEST_CASE("fd_t write", "[fd_t]")
+{
+}
+
+TEST_CASE("fd_t read", "[fd_t]")
+{
 }
