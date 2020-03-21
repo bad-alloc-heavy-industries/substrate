90b11ed53f400712b6f82551301e2d51c59a7da8
Aki Van Ness <aki@lethalbit.net>
2020-03-21 19:42:53 -0400

    Added some more tests for `shift_nibble`

--- test/utility.cxx
+++ test/utility.cxx
@@ -2183,6 +2183,7 @@ TEST_CASE("C++ typename decoding", "[utility]")
 using substrate::shift_nibble;
 TEST_CASE("shift nibble", "[utility]") {
 	uint64_t value{0x000000000000000FLLU};
+	uint64_t value2{0x0123456789ABCDEFLLU};
 
 	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x00000000000000F0LLU);
 	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x0000000000000F00LLU);
@@ -2199,4 +2200,18 @@ TEST_CASE("shift nibble", "[utility]") {
 	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x00F0000000000000LLU);
 	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x0F00000000000000LLU);
 	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0xF000000000000000LLU);
+
+	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0x23456789ABCDEF01LLU);
+	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0x456789ABCDEF0123LLU);
+	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0x6789ABCDEF012345LLU);
+	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0x89ABCDEF01234567LLU);
+	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0xABCDEF0123456789LLU);
+	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0xCDEF0123456789ABLLU);
+	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0xEF0123456789ABCDLLU);
+	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0x0123456789ABCDEFLLU);
+
+	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 4U)) == 0x456789ABCDEF0123LLU);
+	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 4U)) == 0x89ABCDEF01234567LLU);
+	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 4U)) == 0xCDEF0123456789ABLLU);
+	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 4U)) == 0x0123456789ABCDEFLLU);
 }
