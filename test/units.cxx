0290342bb38c4ece294c2fc21f199e18eb542f78
Aki Van Ness <aki@lethalbit.net>
2020-03-20 21:03:10 -0400

    Added some basic SI and IEC units

---
+++ test/units.cxx
@@ -0,0 +1,50 @@
+#include <cstdint>
+#include <substrate/units>
+#include <catch.hpp>
+
+using substrate::operator ""_KiB;
+using substrate::operator ""_MiB;
+using substrate::operator ""_GiB;
+using substrate::operator ""_TiB;
+using substrate::operator ""_PiB;
+
+using substrate::operator ""_KB;
+using substrate::operator ""_MB;
+using substrate::operator ""_GB;
+using substrate::operator ""_TB;
+using substrate::operator ""_PB;
+
+TEST_CASE( "Units", "[utility]") {
+	SECTION( "IEC Units" ) {
+		REQUIRE(128_KiB == 131072);
+		REQUIRE(1024_KiB == 1_MiB);
+
+		REQUIRE(32_MiB == 33554432);
+		REQUIRE(1024_MiB == 1_GiB);
+
+		REQUIRE(512_GiB == 549755813888);
+		REQUIRE(1024_GiB == 1_TiB);
+
+		REQUIRE(19_TiB == 20890720927744);
+		REQUIRE(1024_TiB == 1_PiB);
+
+		REQUIRE(76_PiB == 85568392920039424);
+
+	}
+
+	SECTION( "SI Units" ) {
+		REQUIRE(128_KB == 128000);
+		REQUIRE(1000_KB == 1_MB);
+
+		REQUIRE(32_MB == 32000000);
+		REQUIRE(1000_MB == 1_GB);
+
+		REQUIRE(512_GB == 512000000000);
+		REQUIRE(1000_GB == 1_TB);
+
+		REQUIRE(19_TB == 19000000000000);
+		REQUIRE(1000_TB == 1_PB);
+
+		REQUIRE(76_PB == 76000000000000000);
+	}
+}
