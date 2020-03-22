23230691a8595158dc8c0e6fc1219cafb95f6f46
dx-mon <dx-mon@users.sourceforge.net>
2020-03-22 03:08:05 +0000

    Implemented managedPtr_t<T []>

--- test/managed_ptr.cxx
+++ test/managed_ptr.cxx
@@ -57,3 +57,53 @@ TEST_CASE("managedPtr_t<T>", "[managedPtr_t]")
 	REQUIRE(value.valid());
 	REQUIRE(*value == 'B');
 }
+
+TEST_CASE("managedPtr_t<T []>", "managedPtr_t")
+{
+	REQUIRE_NOTHROW([]()
+	{
+		auto value = make_managed<int32_t []>(2);
+		REQUIRE(bool{value});
+		REQUIRE(value.valid());
+		REQUIRE_FALSE(value[0]);
+		value[0] = 42;
+		REQUIRE_FALSE(value[1]);
+		value[1] = 4000;
+
+		REQUIRE(*value == 42);
+		REQUIRE(value.get());
+
+		const int32_t &ref = value;
+		REQUIRE(&ref == value.get());
+
+		managedPtr_t<int32_t []> invalid{};
+		REQUIRE_FALSE(bool{invalid});
+		REQUIRE_FALSE(invalid.valid());
+		REQUIRE_FALSE(invalid.get());
+
+		value.swap(invalid);
+		REQUIRE_FALSE(value.valid());
+		REQUIRE_FALSE(value.get());
+		REQUIRE(invalid.valid());
+		REQUIRE(invalid.get());
+		REQUIRE(invalid.get() == &ref);
+
+		value.swap(invalid);
+		REQUIRE_FALSE(invalid.valid());
+		REQUIRE(value.valid());
+
+		managedPtr_t<void> elided{std::move(value)};
+		REQUIRE_FALSE(bool{value});
+		REQUIRE(bool{elided});
+		REQUIRE(elided);
+		void *const ptr = elided;
+		REQUIRE(elided.get() == ptr);
+		const void *const constPtr = elided;
+		REQUIRE(elided.get() == constPtr);
+		REQUIRE(elided.get<int32_t>() == &ref);
+	}());
+
+	auto value = make_managed_nothrow<char>('B');
+	REQUIRE(value.valid());
+	REQUIRE(*value == 'B');
+}
