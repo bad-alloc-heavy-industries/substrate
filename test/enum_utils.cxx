a06147da8f69038a0db1318705e435ccb0397745
dx-mon <dx-mon@users.sourceforge.net>
2020-03-30 23:22:01 +0100

    Reformatted the enum_utils tests to be more conforming with the rest of the repo

--- test/enum_utils.cxx
+++ test/enum_utils.cxx
@@ -5,14 +5,15 @@
 #include <substrate/enum_utils>
 #include <catch.hpp>
 
-enum class Flags : uint8_t {
-	None   = 0,
-	Foo    = 1 << 1,
-	Bar    = 1 << 2,
-	Baz    = 1 << 3,
-	Qux    = 1 << 4,
-	Quux   = 1 << 5,
-	Corge  = 1 << 6,
+enum class Flags : uint8_t
+{
+	None = 0,
+	Foo = 1 << 1,
+	Bar = 1 << 2,
+	Baz = 1 << 3,
+	Qux = 1 << 4,
+	Quux = 1 << 5,
+	Corge = 1 << 6,
 	Grault = 1 << 7,
 };
 
@@ -22,33 +23,31 @@ using substrate::extract_flag_pairs;
 using substrate::enum_name;
 using substrate::enum_value;
 
-
-namespace substrate {
-	template<>
-	struct enableEnumBitmask_t<Flags>{
-			static constexpr bool enabled = true;
-	};
+namespace substrate
+{
+	template<> struct enableEnumBitmask_t<Flags>
+		{ static constexpr bool enabled = true; };
 }
 
-const std::array<const enumPair_t<Flags>, 8> flags_s{{
-	{ Flags::None,    "No Flags" },
-	{ Flags::Foo,     "Foo"      },
-	{ Flags::Bar,     "Bar"      },
-	{ Flags::Baz,     "Baz"      },
-	{ Flags::Qux,     "Qux"      },
-	{ Flags::Quux,    "Quux"     },
-	{ Flags::Corge,   "Corge"    },
-	{ Flags::Grault,  "Grault"   },
+const std::array<const enumPair_t<Flags>, 8> flags_s
+{{
+	{Flags::None, "No Flags"},
+	{Flags::Foo, "Foo"},
+	{Flags::Bar, "Bar"},
+	{Flags::Baz, "Baz"},
+	{Flags::Qux, "Qux"},
+	{Flags::Quux, "Quux"},
+	{Flags::Corge, "Corge"},
+	{Flags::Grault, "Grault"},
 }};
 
-
-TEST_CASE( "Flag extraction", "[enum_utils]")
+TEST_CASE("Flag extraction", "[enum_utils]")
 {
 	/* This test isn't the best as it's order-dependent for the flags */
-	SECTION( "Raw flag extraction")
+	SECTION("Raw flag extraction")
 	{
 		Flags tc1 = Flags::Foo | Flags::Baz | Flags::Grault;
-		std::vector<Flags> tc1_exp = { Flags::Foo , Flags::Baz , Flags::Grault };
+		std::vector<Flags> tc1_exp = {Flags::Foo, Flags::Baz, Flags::Grault};
 
 		auto retvec = extract_flags<Flags>(tc1, flags_s);
 
@@ -56,14 +55,15 @@ TEST_CASE( "Flag extraction", "[enum_utils]")
 		REQUIRE(retvec == tc1_exp);
 	}
 
-	SECTION( "Flag pair extraction")
+	SECTION("Flag pair extraction")
 	{
 		Flags tc2 = Flags::Grault | Flags::Quux | Flags::Baz | Flags::Bar;
-		std::vector<enumPair_t<Flags>> tc2_exp = {
-			{ Flags::Grault,  "Grault"   },
-			{ Flags::Quux,    "Quux"     },
-			{ Flags::Baz,     "Baz"      },
-			{ Flags::Bar,     "Bar"      },
+		std::vector<enumPair_t<Flags>> tc2_exp
+		{
+			{Flags::Grault, "Grault"},
+			{Flags::Quux, "Quux"},
+			{Flags::Baz, "Baz"},
+			{Flags::Bar, "Bar"},
 		};
 
 		auto retvec = extract_flag_pairs<Flags>(tc2, flags_s);
@@ -75,15 +75,13 @@ TEST_CASE( "Flag extraction", "[enum_utils]")
 		std::reverse(retvec.begin(), retvec.end());
 
 		REQUIRE(retvec.size() == tc2_exp.size());
-		REQUIRE(std::equal(retvec.begin(),
-					retvec.end(),
-					tc2_exp.begin(),
-					[](const enumPair_t<Flags>& l, const enumPair_t<Flags>& r){
-						return (l.value() == r.value());
-					}));
+		REQUIRE(std::equal(retvec.begin(), retvec.end(), tc2_exp.begin(),
+			[](const enumPair_t<Flags>& l, const enumPair_t<Flags>& r)
+				{ return (l.value() == r.value()); })
+		);
 	}
 
-	SECTION ( "Unknown Flag extraction")
+	SECTION("Unknown Flag extraction")
 	{
 		auto ret = extract_flags<Flags>(Flags::None, flags_s);
 		auto ret2 = extract_flag_pairs<Flags>(Flags::None, flags_s);
@@ -99,68 +97,64 @@ TEST_CASE( "Flag extraction", "[enum_utils]")
 }
 
 
-TEST_CASE( "Enum bitwise operators", "[enum_utils]")
+TEST_CASE("Enum bitwise operators", "[enum_utils]")
 {
-	SECTION ( "Bitwise Or")
+	SECTION("Bitwise Or")
 	{
-		REQUIRE(static_cast<uint8_t>(Flags::Foo    | Flags::Quux)  == ((1U << 1U) | (1U << 5U)));
-		REQUIRE(static_cast<uint8_t>(Flags::Quux   | Flags::Foo)   == ((1U << 5U) | (1U << 1U)));
+		REQUIRE(static_cast<uint8_t>(Flags::Foo | Flags::Quux) == ((1U << 1U) | (1U << 5U)));
+		REQUIRE(static_cast<uint8_t>(Flags::Quux | Flags::Foo) == ((1U << 5U) | (1U << 1U)));
 		REQUIRE(static_cast<uint8_t>(Flags::Grault | Flags::Corge) == ((1U << 7U) | (1U << 6U)));
-		REQUIRE(static_cast<uint8_t>(Flags::Corge  | Flags::None)  == ((1U << 6U) | 0U));
+		REQUIRE(static_cast<uint8_t>(Flags::Corge | Flags::None) == ((1U << 6U) | 0U));
 
 		REQUIRE(static_cast<uint8_t>(Flags::Grault | (1U << 6U)) == ((1U << 7U) | (1U << 6U)));
-		REQUIRE(static_cast<uint8_t>((1U << 5U)    | Flags::Foo)   == ((1U << 5U) | (1U << 1U)));
-
-
+		REQUIRE(static_cast<uint8_t>((1U << 5U) | Flags::Foo) == ((1U << 5U) | (1U << 1U)));
 	}
 
-	SECTION ( "Bitwise And")
+	SECTION("Bitwise And")
 	{
-		REQUIRE(static_cast<uint8_t>(Flags::Foo    & Flags::Quux)  == ((1U << 1) & (1U << 5U)));
-		REQUIRE(static_cast<uint8_t>(Flags::Quux   & Flags::Foo)   == ((1U << 5) & (1U << 1U)));
+		REQUIRE(static_cast<uint8_t>(Flags::Foo & Flags::Quux) == ((1U << 1) & (1U << 5U)));
+		REQUIRE(static_cast<uint8_t>(Flags::Quux & Flags::Foo) == ((1U << 5) & (1U << 1U)));
 		REQUIRE(static_cast<uint8_t>(Flags::Grault & Flags::Corge) == ((1U << 7) & (1U << 6U)));
-		REQUIRE(static_cast<uint8_t>(Flags::Corge  & Flags::None)  == ((1U << 6) & 0U));
+		REQUIRE(static_cast<uint8_t>(Flags::Corge & Flags::None) == ((1U << 6) & 0U));
 
-		REQUIRE(static_cast<uint8_t>(Flags::Foo    & (1U << 5U))  == ((1U << 1) & (1U << 5U)));
-		REQUIRE(static_cast<uint8_t>((1U << 7)     & Flags::Corge) == ((1U << 7) & (1U << 6U)));
-
-
+		REQUIRE(static_cast<uint8_t>(Flags::Foo & (1U << 5U)) == ((1U << 1) & (1U << 5U)));
+		REQUIRE(static_cast<uint8_t>((1U << 7) & Flags::Corge) == ((1U << 7) & (1U << 6U)));
 	}
 
-	SECTION ( "Bitwise Not")
+	SECTION("Bitwise Not")
 	{
-		REQUIRE(static_cast<uint8_t>(~Flags::Foo   )  == static_cast<uint8_t>(~(1U << 1U)));
-		REQUIRE(static_cast<uint8_t>(~Flags::Quux  )  == static_cast<uint8_t>(~(1U << 5U)));
-		REQUIRE(static_cast<uint8_t>(~Flags::Grault)  == static_cast<uint8_t>(~(1U << 7U)));
-		REQUIRE(static_cast<uint8_t>(~Flags::Corge )  == static_cast<uint8_t>(~(1U << 6U)));
+		REQUIRE(static_cast<uint8_t>(~Flags::Foo ) == static_cast<uint8_t>(~(1U << 1U)));
+		REQUIRE(static_cast<uint8_t>(~Flags::Quux ) == static_cast<uint8_t>(~(1U << 5U)));
+		REQUIRE(static_cast<uint8_t>(~Flags::Grault) == static_cast<uint8_t>(~(1U << 7U)));
+		REQUIRE(static_cast<uint8_t>(~Flags::Corge ) == static_cast<uint8_t>(~(1U << 6U)));
 	}
 
-	SECTION ( "Bitwise Xor")
+	SECTION("Bitwise Xor")
 	{
-		REQUIRE(static_cast<uint8_t>(Flags::Foo    ^ Flags::Quux)  == ((1U << 1U) ^ (1U << 5U)));
-		REQUIRE(static_cast<uint8_t>(Flags::Quux   ^ Flags::Foo)   == ((1U << 5U) ^ (1U << 1U)));
+		REQUIRE(static_cast<uint8_t>(Flags::Foo ^ Flags::Quux) == ((1U << 1U) ^ (1U << 5U)));
+		REQUIRE(static_cast<uint8_t>(Flags::Quux ^ Flags::Foo) == ((1U << 5U) ^ (1U << 1U)));
 		REQUIRE(static_cast<uint8_t>(Flags::Grault ^ Flags::Corge) == ((1U << 7U) ^ (1U << 6U)));
-		REQUIRE(static_cast<uint8_t>(Flags::Corge  ^ Flags::None)  == ((1U << 6U) ^ 0U));
+		REQUIRE(static_cast<uint8_t>(Flags::Corge ^ Flags::None) == ((1U << 6U) ^ 0U));
 
-		REQUIRE(static_cast<uint8_t>(Flags::Quux   ^ (1U << 1U))   == ((1U << 5U) ^ (1U << 1U)));
-		REQUIRE(static_cast<uint8_t>((1U << 7U)    ^ Flags::Corge) == ((1U << 7U) ^ (1U << 6U)));
+		REQUIRE(static_cast<uint8_t>(Flags::Quux ^ (1U << 1U)) == ((1U << 5U) ^ (1U << 1U)));
+		REQUIRE(static_cast<uint8_t>((1U << 7U) ^ Flags::Corge) == ((1U << 7U) ^ (1U << 6U)));
 	}
 
-	SECTION ( "Bitwise Or Equ")
+	SECTION("Bitwise Or Equ")
 	{
 		Flags f{Flags::Foo};
 		uint8_t r{1U << 1U};
 		REQUIRE(static_cast<uint8_t>(f |= Flags::Quux) == (r |= (1U << 5U)));
 	}
 
-	SECTION ( "Bitwise And Equ")
+	SECTION("Bitwise And Equ")
 	{
 		Flags f{Flags::Foo};
 		uint8_t r{1U << 1U};
 		REQUIRE(static_cast<uint8_t>(f &= Flags::Quux) == (r &= (1U << 5U)));
 	}
 
-	SECTION ( "Bitwise Xor Equ")
+	SECTION("Bitwise Xor Equ")
 	{
 		Flags f{Flags::Foo};
 		uint8_t r{1U << 1U};
@@ -168,7 +162,7 @@ TEST_CASE( "Enum bitwise operators", "[enum_utils]")
 	}
 }
 
-TEST_CASE( "Enum and string mapping", "[enum_utils]")
+TEST_CASE("Enum and string mapping", "[enum_utils]")
 {
 	REQUIRE(enum_name(flags_s, Flags::Quux) == std::string{"Quux"});
 	REQUIRE(enum_value(flags_s, "Quux") == Flags::Quux);
