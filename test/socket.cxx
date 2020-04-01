146a3224bb586559b16498c433dda1bceed1bc18
dx-mon <dx-mon@users.sourceforge.net>
2020-04-01 23:27:48 +0100

    Added testing for some additional prepare() modes

--- test/socket.cxx
+++ test/socket.cxx
@@ -26,12 +26,15 @@ TEST_CASE("sockaddr prepare", "[socket_t]")
 {
 	const auto ipv4Addr = prepare(socketType_t::ipv4, "127.0.0.1", 0);
 	const auto ipv6Addr = prepare(socketType_t::ipv6, "::1", 0);
+	const auto unknAddr = prepare(socketType_t::dontCare, "", 0);
+	const auto dncAddr = prepare(socketType_t::dontCare, "localhost", 0);
 
 	if (ipv4Addr.ss_family != AF_UNSPEC)
 	{
 		REQUIRE(ipv4Addr.ss_family == AF_INET);
 		const auto addr{as<sockaddr_in>(ipv4Addr)};
 		REQUIRE(addr.sin_addr.s_addr == 0x0100007FU);
+		REQUIRE(addr.sin_port == 0);
 	}
 
 	if (ipv6Addr.ss_family != AF_UNSPEC)
@@ -39,7 +42,11 @@ TEST_CASE("sockaddr prepare", "[socket_t]")
 		REQUIRE(ipv6Addr.ss_family == AF_INET6);
 		const auto addr{as<sockaddr_in6>(ipv6Addr)};
 		REQUIRE(memcmp(&addr.sin6_addr, &in6addr_loopback, sizeof(in6_addr)) == 0);
+		REQUIRE(addr.sin6_port == 0);
 	}
+
+	REQUIRE(unknAddr.ss_family == AF_UNSPEC);
+	REQUIRE((dncAddr.ss_family == AF_INET || dncAddr.ss_family == AF_INET6));
 }
 
 TEST_CASE("socket_t bad socket", "[socket_t]")
