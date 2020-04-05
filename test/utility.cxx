3d499097fd37f2c1aa0d5c64150684aa24b6e1c8
dx-mon <dx-mon@users.sourceforge.net>
2020-04-06 00:42:43 +0100

    Changed the way the shift_nibble test works so the values used can be constexpr

--- test/utility.cxx
+++ test/utility.cxx
@@ -2309,38 +2309,38 @@ TEST_CASE("C++ typename decoding", "[utility]")
 using substrate::shift_nibble;
 TEST_CASE("shift nibble", "[utility]")
 {
-	uint64_t value{0x000000000000000FLLU};
-	uint64_t value2{0x0123456789ABCDEFLLU};
+	constexpr uint64_t value{0x000000000000000FLLU};
+	constexpr uint64_t value2{0x0123456789ABCDEFLLU};
 
-	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x00000000000000F0LLU);
-	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x0000000000000F00LLU);
-	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x000000000000F000LLU);
-	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x00000000000F0000LLU);
-	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x0000000000F00000LLU);
-	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x000000000F000000LLU);
-	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x00000000F0000000LLU);
-	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x0000000F00000000LLU);
-	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x000000F000000000LLU);
-	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x00000F0000000000LLU);
-	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x0000F00000000000LLU);
-	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x000F000000000000LLU);
-	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x00F0000000000000LLU);
-	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0x0F00000000000000LLU);
-	REQUIRE((value = shift_nibble<decltype(value)>(value, 1U)) == 0xF000000000000000LLU);
+	REQUIRE(shift_nibble<decltype(value)>(value, 1U) == 0x00000000000000F0LLU);
+	REQUIRE(shift_nibble<decltype(value)>(value, 2U) == 0x0000000000000F00LLU);
+	REQUIRE(shift_nibble<decltype(value)>(value, 3U) == 0x000000000000F000LLU);
+	REQUIRE(shift_nibble<decltype(value)>(value, 4U) == 0x00000000000F0000LLU);
+	REQUIRE(shift_nibble<decltype(value)>(value, 5U) == 0x0000000000F00000LLU);
+	REQUIRE(shift_nibble<decltype(value)>(value, 6U) == 0x000000000F000000LLU);
+	REQUIRE(shift_nibble<decltype(value)>(value, 7U) == 0x00000000F0000000LLU);
+	REQUIRE(shift_nibble<decltype(value)>(value, 8U) == 0x0000000F00000000LLU);
+	REQUIRE(shift_nibble<decltype(value)>(value, 9U) == 0x000000F000000000LLU);
+	REQUIRE(shift_nibble<decltype(value)>(value, 10U) == 0x00000F0000000000LLU);
+	REQUIRE(shift_nibble<decltype(value)>(value, 11U) == 0x0000F00000000000LLU);
+	REQUIRE(shift_nibble<decltype(value)>(value, 12U) == 0x000F000000000000LLU);
+	REQUIRE(shift_nibble<decltype(value)>(value, 13U) == 0x00F0000000000000LLU);
+	REQUIRE(shift_nibble<decltype(value)>(value, 14U) == 0x0F00000000000000LLU);
+	REQUIRE(shift_nibble<decltype(value)>(value, 15U) == 0xF000000000000000LLU);
 
-	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0x23456789ABCDEF01LLU);
-	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0x456789ABCDEF0123LLU);
-	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0x6789ABCDEF012345LLU);
-	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0x89ABCDEF01234567LLU);
-	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0xABCDEF0123456789LLU);
-	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0xCDEF0123456789ABLLU);
-	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0xEF0123456789ABCDLLU);
-	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 2U)) == 0x0123456789ABCDEFLLU);
+	REQUIRE(shift_nibble<decltype(value2)>(value2, 2U) == 0x23456789ABCDEF01LLU);
+	REQUIRE(shift_nibble<decltype(value2)>(value2, 4U) == 0x456789ABCDEF0123LLU);
+	REQUIRE(shift_nibble<decltype(value2)>(value2, 6U) == 0x6789ABCDEF012345LLU);
+	REQUIRE(shift_nibble<decltype(value2)>(value2, 8U) == 0x89ABCDEF01234567LLU);
+	REQUIRE(shift_nibble<decltype(value2)>(value2, 10U) == 0xABCDEF0123456789LLU);
+	REQUIRE(shift_nibble<decltype(value2)>(value2, 12U) == 0xCDEF0123456789ABLLU);
+	REQUIRE(shift_nibble<decltype(value2)>(value2, 14U) == 0xEF0123456789ABCDLLU);
+	REQUIRE(shift_nibble<decltype(value2)>(value2, 16U) == 0x0123456789ABCDEFLLU);
 
-	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 4U)) == 0x456789ABCDEF0123LLU);
-	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 4U)) == 0x89ABCDEF01234567LLU);
-	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 4U)) == 0xCDEF0123456789ABLLU);
-	REQUIRE((value2 = shift_nibble<decltype(value2)>(value2, 4U)) == 0x0123456789ABCDEFLLU);
+	REQUIRE(shift_nibble<decltype(value2)>(value2, 4U) == 0x456789ABCDEF0123LLU);
+	REQUIRE(shift_nibble<decltype(value2)>(value2, 8U) == 0x89ABCDEF01234567LLU);
+	REQUIRE(shift_nibble<decltype(value2)>(value2, 12U) == 0xCDEF0123456789ABLLU);
+	REQUIRE(shift_nibble<decltype(value2)>(value2, 16U) == 0x0123456789ABCDEFLLU);
 }
 
 using substrate::bswap16;
