8e18a9423cb056a704784e46d140954002d60465
Aki Van Ness <aki@lethalbit.net>
2021-04-03 03:03:14 -0400

    Moved `shift_nibble` and `swap16`/`swap32`/`swap64` into the new `bits` header and moved the tests. Added a new `rotl` and `rotr` to the bits header

---
+++ test/prng.cxx
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: BSD-3-Clause
+#include <cstdint>
+
+#if __cplusplus < 201402L && !defined(SUBSTRATE_CXX11_COMPAT)
+#	define SUBSTRATE_CXX11_COMPAT
+#endif
+#include <substrate/prng>
+#include <catch.hpp>
+
+
+
+/* vim: set ft=cpp ts=4 sw=4 noexpandtab: */
