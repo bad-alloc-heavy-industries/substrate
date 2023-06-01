755436543bfbee1e64cceaa7ff4abea907d231c5
L. E. Segovia <amy@amyspark.me>
2023-06-01 20:15:10 -0300

    sysv_checksum: Implement tests

--- test/hash.cxx
+++ test/hash.cxx
@@ -7,6 +7,17 @@
 #include <substrate/hash>
 #include <catch2/catch_test_macros.hpp>
 
+TEST_CASE("sysv", "[hash]")
+{
+	const std::string empty{};
+	const std::array<uint8_t, 1> zero{{0x0}};
+	const std::string example{"The quick brown fox jumps over the lazy dog"};
+	const std::string example2{"The quick brown fox jumps over the lazy dog."};
 
+	REQUIRE(substrate::sysv_checksum(empty) == 0x0);
+	REQUIRE(substrate::sysv_checksum(zero) == 0x0);
+	REQUIRE(substrate::sysv_checksum(example) == 0xFD9);
+	REQUIRE(substrate::sysv_checksum(example2) == 0x1007);
+}
 
 /* vim: set ft=cpp ts=4 sw=4 noexpandtab: */
